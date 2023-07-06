95551ee43872339c15c8ab1494dae106098ab27a
Ricardo Alves <ricardospalves@gmail.com>
2023-07-06 17:22:02 -0300

    add base

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Ricardo Alves',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
