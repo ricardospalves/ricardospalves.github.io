ec94a6279ba230b309de40ac62b670fab6097728
Ricardo Alves <ricardospalves@gmail.com>
2023-07-07 17:48:15 -0300

    update Bio section

--- src/pages/Home/Bio/Bio.tsx
+++ src/pages/Home/Bio/Bio.tsx
@@ -1,6 +1,6 @@
 export const Bio = () => {
   return (
-    <>
+    <div>
       <h1 className="text-3xl font-bold text-black dark:text-white">
         Ricardo Alves
       </h1>
@@ -8,12 +8,13 @@ export const Bio = () => {
       <h2 className="text-xl font-bold">Desenvolvedor front-end</h2>
 
       <p className="mt-4">
-        Olá, sou o Ricardo, desenvolvedor front-end desde{' '}
+        Olá, sou o <strong>Ricardo</strong>,{' '}
+        <strong>desenvolvedor front-end</strong> desde{' '}
         <time dateTime="2015">2015</time> com uma paixão por criar interfaces
         incríveis e funcionais. Contribuí em mais de 70 projetos (e contando),
         sempre zelando pelas boas práticas, experiência do usuário e
         acessibilidade.
       </p>
-    </>
+    </div>
   )
 }
