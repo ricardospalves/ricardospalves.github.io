9608c6bc252ab7a59121b59c87fa0ad6b71347b3
Ricardo Alves <ricardospalves@gmail.com>
2023-07-07 15:30:54 -0300

    add colors to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        foregroundLightTheme: '#595959',
+        backgroundLightTheme: '#fff',
+        foregroundDarkTheme: '#a9a9a9',
+        backgroundDarkTheme: '#1f1f1f',
+      },
+    },
   },
   plugins: [],
 }
