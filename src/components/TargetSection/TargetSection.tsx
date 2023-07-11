e972119f3fba5e9016ed8149d751c86dc00f550e
Ricardo Alves <ricardospalves@gmail.com>
2023-07-11 17:36:22 -0300

    improve TargetSection component

--- src/components/TargetSection/TargetSection.tsx
+++ src/components/TargetSection/TargetSection.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link'
 import { ReactNode } from 'react'
-import { MdLink as AnchorIcon } from 'react-icons/md'
 import { createIDFromString } from '@/utils/createIDFromString'
+import { Heading } from './Heading/Heading'
 
 export type TargetSectionProps = {
   heading: string
@@ -12,19 +11,8 @@ export const TargetSection = ({ heading, children }: TargetSectionProps) => {
   const id = createIDFromString(heading)
 
   return (
-    <section id={id}>
-      <h2 className="flex items-center gap-2 pb-2 border-b border-current text-xl font-bold text-black dark:text-white">
-        <span className="block grow">{heading}</span>
-
-        <Link
-          className="flex items-center justify-center w-7 h-7 shrink-0 text-blue-800 hover:text-blue-950 focus-visible:text-blue-950 dark:text-sky-400 dark:hover:text-sky-300 dark:focus-visible:text-sky-300"
-          href={`#${id}`}
-          aria-hidden={true}
-          tabIndex={-1}
-        >
-          <AnchorIcon className="block w-full h-full" />
-        </Link>
-      </h2>
+    <section id={id} className="max-w-xl mx-auto px-0 lg:p-4">
+      <Heading href={`#${id}`}>{heading}</Heading>
 
       <div className="px-4 mt-8 space-y-16">{children}</div>
     </section>
