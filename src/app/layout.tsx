6cf74bcc9f36da7b5324a1d56b0e20a1d121a3cc
Ricardo Alves <ricardospalves@gmail.com>
2023-07-11 00:50:01 -0300

    add google verification code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: CONFIG.name,
   description: CONFIG.description,
   robots: 'index,follow',
+  verification: {
+    google: 'co-FFjJZMK_DvOQJSzw9Bz0v4ObsXy_i92hAjlbmBfg',
+  },
   twitter: {
     card: 'summary_large_image',
     site: CONFIG.url,
@@ -37,31 +40,31 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
+          href="./favicons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicons/favicon-32x32.png"
+          href="./favicons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicons/favicon-16x16.png"
+          href="./favicons/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="manifest" href="./favicons/site.webmanifest" />
         <link
           rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
+          href="./favicons/safari-pinned-tab.svg"
           color="#000000"
         />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="shortcut icon" href="favicon.ico" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
           name="msapplication-config"
-          content="/favicons/browserconfig.xml"
+          content="./favicons/browserconfig.xml"
         />
         <meta name="theme-color" content="#000000" />
       </head>
