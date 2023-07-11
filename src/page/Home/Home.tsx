bda4b2cfe471d28319594c6eb9d72db189e54ef9
Ricardo Alves <ricardospalves@gmail.com>
2023-07-11 17:35:23 -0300

    remove css classes from main tag

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -7,7 +7,7 @@ import { CommunicationChannels } from './CommunicationChannels'
 
 export const Home = () => {
   return (
-    <main className="max-w-xl p-4 mx-auto">
+    <main>
       <article className="space-y-16">
         <Bio />
         <CommunicationChannels />
