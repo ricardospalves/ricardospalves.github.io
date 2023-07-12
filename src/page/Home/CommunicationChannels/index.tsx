07318b1680da841caa96181ccc5b09ebceb61852
Ricardo Alves <ricardospalves@gmail.com>
2023-07-12 00:12:26 -0300

    remove CommunicationChannels section

--- src/page/Home/CommunicationChannels/index.tsx
+++
@@ -1,1 +0,0 @@
-export { CommunicationChannels } from './CommunicationChannels'
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -3,14 +3,12 @@ import { Education } from './Education'
 import { Experience } from './Experience'
 import { Skills } from './Skills'
 import { Improvement } from './Improvement'
-import { CommunicationChannels } from './CommunicationChannels'
 
 export const Home = () => {
   return (
     <main>
       <article className="space-y-16">
         <Bio />
-        <CommunicationChannels />
         <Experience />
         <Education />
         <Skills />
