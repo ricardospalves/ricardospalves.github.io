70ea72f92dd20c1a4379def186996c10c6621bc1
Ricardo Alves <ricardospalves@gmail.com>
2023-07-12 11:52:20 -0300

    fix media query classes in TagetSection

--- src/components/TargetSection/TargetSection.tsx
+++ src/components/TargetSection/TargetSection.tsx
@@ -11,7 +11,7 @@ export const TargetSection = ({ heading, children }: TargetSectionProps) => {
   const id = createIDFromString(heading)
 
   return (
-    <section id={id} className="max-w-xl mx-auto px-0 lg:p-4">
+    <section id={id} className="max-w-xl mx-auto px-0 md:p-4">
       <Heading href={`#${id}`}>{heading}</Heading>
 
       <div className="px-4 mt-8 space-y-16">{children}</div>
