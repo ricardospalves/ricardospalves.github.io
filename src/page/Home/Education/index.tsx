41a91d28808b5a0cba8cd897a833858d7cb6e184
Ricardo Alves <ricardospalves@gmail.com>
2024-12-14 03:15:42 -0300

    fix: fix education content

--- src/page/Home/Education/index.tsx
+++ src/page/Home/Education/index.tsx
@@ -1,5 +1,52 @@
 import { Entities } from '@/components/Entities'
 import { Section } from '@/components/Section'
+import { ReactNode } from 'react'
+
+type EducationListItem = {
+  title: ReactNode
+  institution: ReactNode
+  startDate: string
+  endDate: string
+}
+
+const EDUCATION_LIST: EducationListItem[] = [
+  {
+    title: 'Tecnologia em Sistemas para Internet',
+    institution: 'UNINOVE',
+    startDate: '2015-07-01T00:00',
+    endDate: '2018-08-03T00:00',
+  },
+  {
+    title: 'Técnico em Informática',
+    institution: 'ETEC',
+    startDate: '2012-06-01T00:00',
+    endDate: '2013-12-18T00:00',
+  },
+  {
+    title: 'HTML5 Fundamentos',
+    institution: 'Impacta Tecnologia',
+    startDate: '2014-04-28T00:00',
+    endDate: '2014-05-14T00:00',
+  },
+  {
+    title: 'CSS3 Fundamentos',
+    institution: 'Impacta Tecnologia',
+    startDate: '2014-07-14T00:00',
+    endDate: '2014-07-25T00:00',
+  },
+  {
+    title: 'JavaScript Ninja',
+    institution: 'Fernando Daciuk',
+    startDate: '2018T00:00',
+    endDate: '2018T00:00',
+  },
+  {
+    title: <em lang="en">The Node.js Master Class</em>,
+    institution: 'Pirple',
+    startDate: '2024T00:00',
+    endDate: '2024T00:00',
+  },
+]
 
 export const Education = () => {
   return (
@@ -8,139 +55,36 @@ export const Education = () => {
         <Section.Heading as="h2">Educação</Section.Heading>
 
         <Entities.Root>
-          <Entities.Card>
-            <Entities.CardAvatar>
-              <svg
-                viewBox="0 0 24 24"
-                className="block size-full fill-background"
-              >
-                <circle cx={12} cy={12} r={12} />
-              </svg>
-            </Entities.CardAvatar>
+          {EDUCATION_LIST.map(({ endDate, institution, startDate, title }) => {
+            const key = `${title}:${institution}`
 
-            <Entities.CardContent>
-              <Entities.CardHeading as="h3">
-                Tecnologia em Sistemas para Internet na <b>UNINOVE</b>
-              </Entities.CardHeading>
+            return (
+              <Entities.Card key={key}>
+                <Entities.CardAvatar>
+                  <svg
+                    viewBox="0 0 24 24"
+                    className="block size-full fill-background"
+                  >
+                    <circle cx={12} cy={12} r={12} />
+                  </svg>
+                </Entities.CardAvatar>
 
-              <Entities.CardTimeRange
-                startISODate="2015T00:00"
-                endISODate="2017T00:00"
-                formatTemplate="yyyy"
-              />
-            </Entities.CardContent>
-          </Entities.Card>
+                <Entities.CardContent>
+                  <Entities.CardTitle as="h3">{title}</Entities.CardTitle>
 
-          <Entities.Card>
-            <Entities.CardAvatar>
-              <svg
-                viewBox="0 0 24 24"
-                className="block size-full fill-background"
-              >
-                <circle cx={12} cy={12} r={12} />
-              </svg>
-            </Entities.CardAvatar>
+                  <Entities.CardInstitution>
+                    {institution}
+                  </Entities.CardInstitution>
 
-            <Entities.CardContent>
-              <Entities.CardHeading as="h3">
-                Técnico em Informática na <b>ETEC</b>
-              </Entities.CardHeading>
-
-              <Entities.CardTimeRange
-                startISODate="2012T00:00"
-                endISODate="2013T00:00"
-                formatTemplate="yyyy"
-              />
-            </Entities.CardContent>
-          </Entities.Card>
-
-          <Entities.Card>
-            <Entities.CardAvatar>
-              <svg
-                viewBox="0 0 24 24"
-                className="block size-full fill-background"
-              >
-                <circle cx={12} cy={12} r={12} />
-              </svg>
-            </Entities.CardAvatar>
-
-            <Entities.CardContent>
-              <Entities.CardHeading as="h3">
-                HTML5 Fundamentos na <b>Impacta Tecnologia</b>
-              </Entities.CardHeading>
-
-              <Entities.CardTimeRange
-                startISODate="2014T00:00"
-                formatTemplate="yyyy"
-              />
-            </Entities.CardContent>
-          </Entities.Card>
-
-          <Entities.Card>
-            <Entities.CardAvatar>
-              <svg
-                viewBox="0 0 24 24"
-                className="block size-full fill-background"
-              >
-                <circle cx={12} cy={12} r={12} />
-              </svg>
-            </Entities.CardAvatar>
-
-            <Entities.CardContent>
-              <Entities.CardHeading as="h3">
-                CSS3 Fundamentos na <b>Impacta Tecnologia</b>
-              </Entities.CardHeading>
-
-              <Entities.CardTimeRange
-                startISODate="2014T00:00"
-                formatTemplate="yyyy"
-              />
-            </Entities.CardContent>
-          </Entities.Card>
-
-          <Entities.Card>
-            <Entities.CardAvatar>
-              <svg
-                viewBox="0 0 24 24"
-                className="block size-full fill-background"
-              >
-                <circle cx={12} cy={12} r={12} />
-              </svg>
-            </Entities.CardAvatar>
-
-            <Entities.CardContent>
-              <Entities.CardHeading as="h3">
-                JavaScript na <b>Udemy</b>
-              </Entities.CardHeading>
-
-              <Entities.CardTimeRange
-                startISODate="2018T00:00"
-                formatTemplate="yyyy"
-              />
-            </Entities.CardContent>
-          </Entities.Card>
-
-          <Entities.Card>
-            <Entities.CardAvatar>
-              <svg
-                viewBox="0 0 24 24"
-                className="block size-full fill-background"
-              >
-                <circle cx={12} cy={12} r={12} />
-              </svg>
-            </Entities.CardAvatar>
-
-            <Entities.CardContent>
-              <Entities.CardHeading as="h3">
-                <em lang="en">The Node.js Master Class</em> na <b>Pirple.com</b>
-              </Entities.CardHeading>
-
-              <Entities.CardTimeRange
-                startISODate="2024T00:00"
-                formatTemplate="yyyy"
-              />
-            </Entities.CardContent>
-          </Entities.Card>
+                  <Entities.CardTimeRange
+                    startISODate={startDate}
+                    endISODate={endDate}
+                    formatTemplate="MMMM 'de' yyyy"
+                  />
+                </Entities.CardContent>
+              </Entities.Card>
+            )
+          })}
         </Entities.Root>
       </Section.Container>
     </Section.Root>
