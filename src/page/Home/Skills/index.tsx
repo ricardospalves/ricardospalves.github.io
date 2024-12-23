2e09cab48daf1d617744484328402fdec145e973
Ricardo Alves <ricardospalves@gmail.com>
2024-12-23 01:20:30 -0300

    fix: add horizontal padding to Skills section

--- src/page/Home/Skills/index.tsx
+++ src/page/Home/Skills/index.tsx
@@ -7,33 +7,35 @@ export const Skills = () => {
       <Section.Container>
         <Section.Heading as="h2">Habilidades</Section.Heading>
 
-        <ul className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-2 text-highlightForeground">
-          {SKILLS_LIST.map(({ Icon, name }) => {
-            return (
-              <li
-                className="flex items-center justify-center flex-col aspect-square border border-current rounded"
-                key={name}
-              >
-                <span className="block mt-2">{name}</span>
+        <div className="px-2 lg:px-0">
+          <ul className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-2 text-center text-highlightForeground">
+            {SKILLS_LIST.map(({ Icon, name }) => {
+              return (
+                <li
+                  className="flex items-center justify-center flex-col aspect-square border border-current rounded"
+                  key={name}
+                >
+                  <span className="block mt-2">{name}</span>
 
-                <Icon
-                  className="block mx-auto size-12 order-first"
-                  aria-hidden={true}
-                />
-              </li>
-            )
-          })}
-        </ul>
+                  <Icon
+                    className="block mx-auto size-12 order-first"
+                    aria-hidden={true}
+                  />
+                </li>
+              )
+            })}
+          </ul>
 
-        <p className="mt-8 mb-4 text-xl">Habilidades relacionadas:</p>
+          <p className="mt-8 mb-4 text-xl">Habilidades relacionadas:</p>
 
-        <ul className="pl-8 list-disc">
-          <li>Responsive Web Design;</li>
-          <li>UX (User Experience);</li>
-          <li>UI design;</li>
-          <li>Usabilidade;</li>
-          <li>Acessibilidade.</li>
-        </ul>
+          <ul className="pl-8 list-disc">
+            <li>Responsive Web Design;</li>
+            <li>UX (User Experience);</li>
+            <li>UI design;</li>
+            <li>Usabilidade;</li>
+            <li>Acessibilidade.</li>
+          </ul>
+        </div>
       </Section.Container>
     </Section.Root>
   )
